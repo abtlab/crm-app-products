295ab7420b76454ad964b9caa7d5a37adad3db6d
Usman Ajupov <au073@mail.ru>
2019-02-03 21:58:47 +0300

    fixe

--- src/controllers/Statuses.go
+++ src/controllers/Statuses.go
@@ -1,24 +1,22 @@
 package products
 
 import (
+	"../types/statuses"
 	"encoding/json"
 	"net/http"
 )
 
-import "../types/statuses"
-
 // Get :
 func Get(writer http.ResponseWriter, request *http.Request) {
-	var id = request.URL.Query()["id"][0]
-
-	var productStatus = products.ProductStatus{ID: id, Name: "Product status 1"}
+	id := request.URL.Query()["id"][0]
+	productStatus := products.ProductStatus{ID: id, Name: "Product status 1"}
 
 	json.NewEncoder(writer).Encode(productStatus)
 }
 
 // GetList :
 func GetList(writer http.ResponseWriter, request *http.Request) {
-	var productStatuses = []products.ProductStatus{
+	productStatuses := []products.ProductStatus{
 		{ID: "1", Name: "Product status 1"},
 		{ID: "2", Name: "Product status 2"},
 	}
